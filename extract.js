3c8662211d5369df1b3e5ee6c25b5d271064c9cb
zefeiO <ouzefei1551@gmail.com>
2021-09-02 13:57:09 -0400

    deleted example folder

--- extract.js
+++ extract.js
@@ -1,15 +1,17 @@
 var playInfo = window.__playinfo__;
-const videos = playInfo["dash"]["video"];
-var videoUrls = [];
-for(var i = 0; i < videos.length; i++){
-    videoUrls.push(videos[i]["baseUrl"])
-}
-console.log(videoUrls);
-var node = document.getElementsByTagName("body")[0];
+if(playInfo){
+    const videos = playInfo["dash"]["video"];
+    var videoUrls = [];
+    for(var i = 0; i < videos.length; i++){
+        videoUrls.push(videos[i]["baseUrl"])
+    }
+    console.log(videoUrls);
+    var node = document.getElementsByTagName("body")[0];
 
-for(var i = 0; i < videoUrls.length; i++){
-    var elem = document.createElement("div");
-    elem.setAttribute("id", "video" + i);
-    elem.innerHTML = videoUrls[i];
-    node.appendChild(elem);
+    for(var i = 0; i < videoUrls.length; i++){
+        var elem = document.createElement("div");
+        elem.setAttribute("id", "video" + i);
+        elem.innerHTML = videoUrls[i];
+        node.appendChild(elem);
+    }
 }
